367453e2a3a71937d99750a17c1ba64ed469a9c3
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-02-12 11:08:39 +0200

    task3

---
+++ src/js/03-promises-2.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('input[name="delay"]');
+const inputStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]');
+
+form.addEventListener('submit', submitForCreatePromise);
+form.addEventListener('change', setOptions);
+
+let delay = null;
+let step = null;
+let amount = null;
+
+function setOptions() {
+  delay = +inputDelay.value;
+  step = +inputStep.value;
+  amount = +inputAmount.value;
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject;
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function startCraetPromise() {
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
+
+function submitForCreatePromise(e) {
+  e.preventDefault();
+
+  startCraetPromise();
+}
